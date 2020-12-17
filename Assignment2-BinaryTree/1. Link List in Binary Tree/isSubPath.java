83946a030c99800b035ce071b8af05d41437a0af
xxpp1314 <37097790+xxpp1314@users.noreply.github.com>
2020-12-16 22:09:49 -0800

    Create isSubPath.java

---
+++ Assignment2-BinaryTree/1. Link List in Binary Tree/isSubPath.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        if(head == null){
+            return true;
+        }
+        if(root == null){
+            return false;
+        }
+        return isSub(head, root) || isSubPath(head, root.left) || isSubPath(head, root.right);
+    }
+    
+    public boolean isSub(ListNode head, TreeNode node){
+        if(head == null){
+            return true;
+        }
+        if(node == null){
+            return false;
+        }
+        if(head.val != node.val){
+            return false;
+        }
+        return isSub(head.next, node.left) || isSub(head.next, node.right);
+    }
+}
+//idea:recursion
